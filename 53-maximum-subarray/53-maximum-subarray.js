24638f8eb9c9509e3504d6a5766f5a9879b82b98
Rusty Porter <87335128+7thPorter@users.noreply.github.com>
2022-06-12 22:10:54 -0700

    Time: 148 ms (12.75%), Space: 50.2 MB (69.82%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const maxSubArray = function(nums) {
+    let maxSum = nums[0];
+    let currentSum = 0;
+    
+    for (let i = 0; i < nums.length; i ++) {
+        if (currentSum < 0) {
+            currentSum = 0;
+        }
+        currentSum += nums[i];
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    
+    return maxSum;
+};
