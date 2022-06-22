7c2a61fc231c396d2c919e5f694f6b18b472935c
Rusty Porter <87335128+7thPorter@users.noreply.github.com>
2022-06-21 21:08:04 -0700

    Time: 142 ms (5.47%), Space: 43.3 MB (21.00%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let pointer1 = 0;
+    let pointer2 = numbers.length - 1;
+    console.log(pointer1, pointer2);
+    
+    while (pointer1 < pointer2) {
+        if (numbers[pointer1] + numbers[pointer2] === target) {
+            return [pointer1 + 1, pointer2 + 1];
+        } else if (numbers[pointer1] + numbers[pointer2] < target) {
+            pointer1 ++;
+        } else {
+            pointer2 --;
+        }
+    }
+};
